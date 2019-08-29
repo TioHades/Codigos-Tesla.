f6ac48bafe062a44de7db2cf00e31af45b34b636
Colgate13 <54006467+Colgate13@users.noreply.github.com>
2019-08-28 21:37:49 -0300

    Add files via upload

---
+++ Verde_Robo_Beta_9_Da_Amanha.ino
@@ -0,0 +1,68 @@
+#include <Ultrasonic.h>
+#include <Servo.h>
+#define fita_1 980 //refletancia da linha preta
+#define fita_2 1023// refletancia da linha preta
+#define SERVO 6
+
+Ultrasonic ultrassom(12, 13);//Varievel do tipo, Ultrassonico.
+Servo myservo;//Variavel do tipo Servo.
+int pos;//varievel posição do servo.
+
+float sensorUltra;//Sensor Ultrassonico.
+int pinoSensor = A5;
+int valor = 0;//Sensor de Refletancia
+int led_1 = 7;
+int led_2 = 4;
+
+void Ultrasonico() {
+  sensorUltra = ultrassom.read();// ultrassom.Ranging(CM) retorna a distancia em
+  Serial.print(sensorUltra); //imprime o valor da variável distancia
+  Serial.println(" cm");
+  //delay(1000);
+  if(sensorUltra <= 20){
+    digitalWrite(led_1, LOW);
+    //   myservo.write(val);
+    for (pos = 0; pos < 120; pos++)
+    {
+      myservo.write(pos);
+      delay(15);
+    }
+    delay(1000);
+    for (pos = 90; pos >= 0; pos--)
+    {
+      myservo.write(pos);
+      delay(15);
+      delay(15);
+    }
+  }else if(sensorUltra > 20){
+    digitalWrite(led_1, HIGH);
+  }
+}
+
+void SensorRefletancia(){
+  valor = analogRead(pinoSensor);
+  int valorCorrigido = valor;
+  Serial.print("refletancia:");
+  Serial.println(valorCorrigido);
+  delay(100);
+  if(valorCorrigido <= fita_1)
+  {// || //valorCorrigido <= fita_2){
+  digitalWrite(led_2, HIGH);
+  }else {
+    digitalWrite(led_2, LOW);
+  }
+}
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(SERVO);
+  myservo.write(0);
+  pinMode(led_1, OUTPUT);
+  pinMode(led_2, OUTPUT);
+ // pinMode(sensorRefle, INPUT);
+}
+
+void loop() {
+  Ultrasonico();
+  SensorRefletancia();
+
+}
