1f3baa9b058652ec209bcb35b8b9f2ca4b1b700e
Colgate13 <54006467+Colgate13@users.noreply.github.com>
2019-11-19 18:02:45 -0300

    Add files via upload

---
+++ Sumo_Pronto.ino
@@ -0,0 +1,69 @@
+#include <Ultrasonic.h>//BIBLIOTECA - ULTRASSONICO.
+#define fita 1000
+
+Ultrasonic ultrassom1(12, 13);
+Ultrasonic ultrassom2(14, 15);
+int IN1 = 4;
+int IN2 = 5;
+int IN3 = 6;
+int IN4 = 7;
+float sensorUltra1;
+float sensorUltra2;
+
+int pinoSensor1 = A5;
+int valor1 = 0;//Sensor de Refletancia
+
+int pinoSensor2 = A5;
+int valor2 = 0;//Sensor de Refletancia
+
+void Ultrasonico() {
+  sensorUltra1 = ultrassom1.Ranging(CM);// ultrassom.Ranging(CM) retorna a distancia em
+  Serial.print(sensorUltra1); //imprime o valor da variável distancia
+  Serial.println(" cm");
+
+  sensorUltra2 = ultrassom2.Ranging(CM);// ultrassom.Ranging(CM) retorna a distancia em
+  Serial.print(sensorUltra2); //imprime o valor da variável distancia
+  Serial.println(" cm");
+
+  if((sensorUltra1 <= 20) || (sensorUltra2 <= 20)){
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+    }
+}
+
+void SensorRefletancia() {
+
+  valor1 = analogRead(pinoSensor1);
+  int valorCorrigido1 = valor1;
+  Serial.print("refletancia:");
+  Serial.println(valorCorrigido1);
+
+  valor2 = analogRead(pinoSensor2);
+  int valorCorrigido2 = valor2;
+  Serial.print("refletancia:");
+  Serial.println(valorCorrigido2);
+
+  if((valor1 >= fita) || (valor2 >= fita)){
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  }
+}
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  Ultrasonico();
+  SensorRefletancia();
+
+}
